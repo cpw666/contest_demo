d5bcf6452ed1faa97e9b140102ff74bc2ea97651
cpw <cpwei120@sina.com>
2018-09-22 07:23:26 +0800

    ping

--- src/main/java/com/unicom/microserv/contest/contest_demo/web/HellowController.java
+++ src/main/java/com/unicom/microserv/contest/contest_demo/web/HellowController.java
@@ -1,16 +1,28 @@
 package com.unicom.microserv.contest.contest_demo.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/contest/demo/")
 public class HellowController {
-    @RequestMapping("hello")
+    @RequestMapping(
+            value = "hello",
+            method = RequestMethod.GET
+    )
     public String hello(
             @RequestParam String name
     ) {
         return "Hello World: " + name;
+    }
+
+    @RequestMapping(
+            value = "ping",
+            method = RequestMethod.GET
+    )
+    public String ping() {
+        return "pong" + "chenpw8";
     }
 }
